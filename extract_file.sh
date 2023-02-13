4d4a383809a2524cc31a0ede0e76fda009cabfd3
Thiago Murakami <thiago_murakami@hotmail.com>
2023-02-13 16:54:53 -0300

    config sh file

--- extract_file.sh
+++ extract_file.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 set -e
-aws s3 cp s3://stack-app-project-1/app/app_libraries.zip app_libraries.zip
+aws s3 cp s3://s3-demo-sp/app/app_libraries.zip app_libraries.zip
 tar -xzf app_libraries.zip -C /home/hadoop
